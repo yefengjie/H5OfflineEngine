d2add53c3109fe4875d71117c09b7c2ffe977fee
feng ye <yefeng.freedom@gmail.com>
2019-12-17 18:34:48 +0800

    add new api

--- h5_offline_engine/src/main/java/com/yefeng/h5_offline_engine/H5OfflineEngine.kt
+++ h5_offline_engine/src/main/java/com/yefeng/h5_offline_engine/H5OfflineEngine.kt
@@ -2,6 +2,7 @@ package com.yefeng.h5_offline_engine
 
 import android.content.Context
 import android.net.Uri
+import android.os.Build
 import android.webkit.WebResourceResponse
 import java.io.File
 
@@ -67,14 +68,46 @@ object H5OfflineEngine {
             if (!file.exists()) {
                 return null
             }
-            return WebResourceResponse(
-                context.contentResolver.getType(uri),
-                "UTF-8",
-                file.inputStream()
-            )
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                WebResourceResponse(
+                    getMime(uri),
+                    "UTF-8",
+                    200,
+                    "OK",
+                    HashMap<String, String>(),
+                    file.inputStream()
+                )
+            } else {
+                WebResourceResponse(
+                    context.contentResolver.getType(uri),
+                    "UTF-8",
+                    file.inputStream()
+                )
+            }
         }.onFailure { exception ->
             exception.printStackTrace()
         }
         return null
+    }
+
+    private fun getMime(currentUri: Uri): String? {
+        var mime = "text/html"
+        val path = currentUri.path
+        if (path.isNullOrBlank()) {
+            return mime
+        }
+        if (path.endsWith(".css")) {
+            mime = "text/css"
+        } else if (path.endsWith(".json")) {
+            mime = "application/json"
+        } else if (path.endsWith(".js")) {
+            mime = "application/x-javascript"
+        } else if (path.endsWith(".jpg") || path.endsWith(".gif") ||
+            path.endsWith(".png") || path.endsWith(".jpeg") ||
+            path.endsWith(".webp") || path.endsWith(".bmp")
+        ) {
+            mime = "image/*"
+        }
+        return mime
     }
 }
